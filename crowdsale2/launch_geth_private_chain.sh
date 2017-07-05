47ae284b9a76cb347574c84e2ab9c2c32fc5f204
Alexandre Kurth <alexandre.kurth@gmail.com>
2017-07-05 12:16:02 +0200

    fix link between commands and genesis

--- crowdsale2/launch_geth_private_chain.sh
+++ crowdsale2/launch_geth_private_chain.sh
@@ -1,21 +1,2 @@
-geth --datadir ~/.ethereum/bnp999 init ./genesis999.json
-geth --datadir ~/.ethereum/bnp999 --networkid 999 console
-
-
-In another terminal ('CTRL + Shift + o' with terminator to split horizontally your terminal)
-
-    geth attach ~/.ethereum/bnp999/geth.ipc
-
-You are in the geth console, you can type command after '>' letter
-
-    personal.newAccount()
-
-Verify your coinbase
-    eth.coinbase
-
-Start mining
-    miner.start(1)
-
-In another terminal ('CTRL + Shift + e' with terminator to split vertically your terminal)
-
-    mist --rpc ~/.ethereum/bnp999/geth.ipc
+geth --datadir ~/.ethereum/bnp9991 init ./genesis1.json
+geth --datadir ~/.ethereum/bnp9991 --networkid 9991 console
